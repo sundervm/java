662f2aba9581e0e1333b9263f2d424bb4c683b3d
sundervm <thesu02@gmail.com>
2016-02-22 00:54:20 +0530

    commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+/*****************************************
+ * Author:Sunder 
+ * Place : Vmware,Bangalore
+ * Date  : 21-02-2016
+ * 
+ * ***************************************/
+
+  
+  import javax.swing.JOptionPane;
+  
+  
+    class Helloworld{
+	  
+	  public static void main(String[] args){
+		  
+		  JOptionPane.showMessageDialog(null, "Hello, world!");
+		  		  		  
+	  }
+	    
+	  	  
+  }
